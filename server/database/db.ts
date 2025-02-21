8f49a5b0106e24bf29c281ed9e16b73fbc69d74a
Adrien Zaganelli <adrienzaganelli@gmail.com>
2025-02-21 22:08:53 +0100

    schema in drizzle db

--- server/database/db.ts
+++ server/database/db.ts
@@ -2,6 +2,7 @@ import { PGlite } from '@electric-sql/pglite';
 import { drizzle } from 'drizzle-orm/pglite';
 import path from "node:path";
 import { fileURLToPath } from "node:url";
+import * as schema from "./schemas"
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
@@ -9,4 +10,4 @@ const __dirname = path.dirname(__filename);
 const client = new PGlite({
   dataDir: path.resolve(__dirname, "../../.data")
 });
-export const db = drizzle({ client });
+export const db = drizzle({ client, schema});
