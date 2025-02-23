6879fb428168bc78bb122a490dff1affb9970771
Adrien Zaganelli <adrienzaganelli@gmail.com>
2025-02-23 19:17:13 +0100

    add some swagger param annotations

--- server/api/list-users.ts
+++ server/api/list-users.ts
@@ -1,6 +1,14 @@
 import { db } from "../database/db"
 import { user } from "../database/schemas"
 import { defineEventHandler } from "h3"
+import { defineRouteMeta } from "nitropack/runtime"
+
+defineRouteMeta({
+  openAPI: {
+    description: 'Test route description',
+    parameters: [{ in: "query", name: "test", required: true }],
+  },
+});
 
 export default defineEventHandler(async (event) => {
   return await db.select().from(user);
